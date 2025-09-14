6e3636911cb7be80da43052808874fcbe714d3ab
Paulo Ariel Pareja <paulo.pareja@hotmail.es>
2025-09-14 17:32:40 -0300

    replace import

--- src/public-links/public-links.module.ts
+++ src/public-links/public-links.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PublicLinksService } from './public-links.service';
 import { PublicLink } from './entities/public-link.entity';
-import { Note } from 'src/notes/entities/note.entity';
+import { Note } from '../notes/entities/note.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PublicLink, Note])],
